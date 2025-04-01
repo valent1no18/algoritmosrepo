5e88736c37452545d1abbbab0512229477fe9853
valentino_laiño <vitolaino123@gmail.com>
2025-04-01 00:38:18 -0300

    -

--- recursividad_producto.py
+++ recursividad_producto.py
@@ -10,13 +10,3 @@ def producto(n,m):
 print("producto:", producto(2,1))
 
 
-#potencia(m, n)= m ** potencia(m, n - 1) --> potencia(m, n) n = 1 = m
-
-def potencia(m,n):
-    
-    if n == 1:
-        return m
-    else:
-        return m * potencia(m, n - 1)
-    
-print("potencia:", potencia(2,1))
